c522c01e535a2f9b0045bb1903c71938a6b5e00e
xyz-mail1 <sunday.september@protonmail.com>
2024-11-02 12:21:51 +0530

    testing

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
+const apiUrl = "https://api.waifu.im/search?included_tags=ass";
 const fetch = require("node-fetch");
 require("dotenv").config();
-(async () => {
-  const { url } = await (
-    await fetch("https://api.waifu.pics/nsfw/waifu")
-  ).json();
-  console.log(url);
-})();
+const headers = new Headers();
+headers.append("Accept-Version", "v5");
+headers.append("Authorization", `Bearer ${process.env.token}`);
+
+fetch(apiUrl, { headers })
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Request failed with status code: " + response.status);
+    }
+  })
+  .then((data) => {
+    // Process the response data as needed
+    console.log(data.images[0].url);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error.message);
+  });
